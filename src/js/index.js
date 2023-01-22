68ca7a7186cdd9bf8171b808f90d8a62d6775a84
Ana Cristina <122562146+anacristinademoura@users.noreply.github.com>
2023-01-21 22:22:39 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ const botaoTrailer = document.querySelector(".botao-trailer");
 const botaoFecharModal = document.querySelector(".fechar-modal");
 const video = document.getElementById("video");
 const modal = document.querySelector(".modal");
-const linkDoVideo = video.scr;
+const linkDoVideo = video.src;
 
 function alternarModal(){
     modal.classList.toggle("aberto");
